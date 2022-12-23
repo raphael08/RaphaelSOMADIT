1482616b803f5c562eb72ec08069157a3f0e9d02
raphael08 <rsiphael@gmail.com>
2022-12-23 04:49:28 +0300

    setup

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
-import setuptools
-setuptools.setup(
+from setuptools import setup,find_packages
+
+setup(
  name='RaphaelSomaDIT', 
  version='0.0.1',
  author="Raphael Sifael Mkilya",
  author_email="rsiphael@gmail.com",
  description="A SOMA API for obtaining student information of DIT",
- packages=setuptools.find_packages(),
+ url='https://github.com/raphael08/RaphaelSOMADIT.git',
+ zip_safe=False,
+ packages=find_packages(),
  classifiers=[
  "Programming Language :: Python :: 3",
  "License :: OSI Approved :: MIT License",
  "Operating System :: OS Independent",
  ],
- url='https://github.com/raphael08/RaphaelSOMADIT.git'
- zip_safe=False
+ 
 )
